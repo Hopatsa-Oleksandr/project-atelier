b09fe9cc666300e7912160e79f5c1fd220ad538d
oxana1510 <oksi666@xaker.ru>
2018-12-13 14:59:59 +0300

    .

--- js/min.js
+++ js/min.js
@@ -7,3 +7,55 @@ $(document).ready(function(){
   cssEase: 'linear'
   });
 });
+
+$('.slider_responsive').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+ $('.slideBig').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  fade: true,
+  asNavFor: '.slide_photo'
+});
+$('.slide_photo').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slideBig',
+  dots: true,
+  centerMode: false,
+  focusOnSelect: true,
+  vertical: true,
+});
